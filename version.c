985aa6d0f485b5be6c62ab8a73265cbe723607fb
n-t-roff <troff@arcor.de>
2016-03-28 21:18:50 +0200

    sc.doc: Some hyperlinks added

--- version.c
+++ version.c
@@ -2,6 +2,8 @@
  * CODE REVISION NUMBER:
  *
  * The part after the first colon, except the last char, appears on the screen.
+ * The part after the first underscore is the subrevision.
+ * A second underscore marks a development version.
  */
 
-char *rev = "$Revision: 7.16 $";
+char *rev = "$Revision: 7.16_1.0.1_r160328 $";
